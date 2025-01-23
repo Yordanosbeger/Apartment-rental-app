a50630b1fbd837b4b87e630795ab94c524f043d6
Esther Toyin <amooesther2503@gmail.com>
2025-01-23 10:48:16 +0000

    property page footer and landlord profile creation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,54 +1,4 @@
 
-
-
-// import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faHouse } from '@fortawesome/free-solid-svg-icons';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         {/* Add Font Awesome Icon */}
-//         <h1>
-//           <FontAwesomeIcon icon={faHouse} style={{ marginRight: '10px', color: '#00796B' }} />
-//           Welcome to My Project
-//         </h1>
-//         <p>Building visually appealing and centralized content is easy!</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
-
-
-// import React from 'react';
-// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-// import { faHouse } from '@fortawesome/free-solid-svg-icons';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       {/* Icon Container with Inline Style */}
-//       <div
-//         className="icon-container"
-//         style={{ color: 'green', position: 'absolute', top: '10px', right: '10px', fontSize: '24px' }}
-//       >
-//         <FontAwesomeIcon icon={faHouse} />
-//       </div>
-//       <header className="App-header">
-//         <h1>Welcome to My Project</h1>
-//         <p>Building visually appealing and centralized content is easy!</p>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHouse } from '@fortawesome/free-solid-svg-icons';
@@ -57,6 +7,7 @@ import Home from '../src/Pages/Home'
 import Profile from './Pages/Profile';
 import ProfileCreation from './Pages/ProfileCreation';
 import Property from './Pages/Property';
+import ProfileCreationLandLord from './Pages/ProfileCreationLandLord';
 
 
 function App() {
@@ -65,8 +16,7 @@ function App() {
       <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
-      <Route path="/create-profile" element={<ProfileCreation />} />
-      <Route path="/property" element={<Property />} />
+      <Route path="/create-Landlord" element={<ProfileCreationLandLord />} />      <Route path="/create-profile" element={<ProfileCreation />} /><Route path="/property" element={<Property />} />
       </Routes>  
 
     </div>
--- frontend/src/Pages/Property.jsx
+++ frontend/src/Pages/Property.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import PropertyHeader from '../Components/PropertyHeader'
 import PropertyListing from '../Components/PropertyListing'
+import PropertyFooter from '../Components/PropertyFooter'
 
 const Property = () => {
   return (
     <div>
         <PropertyHeader/>
         <PropertyListing/>
+        <PropertyFooter/>
     </div>
   )
 }
