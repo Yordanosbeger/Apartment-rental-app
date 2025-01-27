329babb0b3dcb13346bd6134438fa3113e784b7c
Esther Toyin <amooesther2503@gmail.com>
2025-01-27 15:16:07 +0000

    property detail page header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ import Profile from './Pages/Profile';
 import ProfileCreation from './Pages/ProfileCreation';
 import Property from './Pages/Property';
 import ProfileCreationLandLord from './Pages/ProfileCreationLandLord';
+import PropertyDetail from './Pages/PropertyDetail';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/profile" element={<Profile />} />
       <Route path="/create-Landlord" element={<ProfileCreationLandLord />} />      <Route path="/create-profile" element={<ProfileCreation />} /><Route path="/property" element={<Property />} />
+      <Route path="/property-detail" element={<PropertyDetail />} />
       </Routes>  
 
     </div>
---
+++ frontend/src/Pages/PropertyDetail.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import PropertyDetailHeader from '../Components/ProppertyDetailHeader'
+
+const PropertyDetail = () => {
+  return (
+    <div>
+        <PropertyDetailHeader/>
+    </div>
+  )
+}
+
+export default PropertyDetail
